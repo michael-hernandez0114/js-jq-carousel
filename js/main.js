6073912cece10343bc4302c32d4e60c774a16313
Michael Hernandez <gammabeta010@gmail.com>
2020-03-02 22:38:24 +0100

    Finito esercizio basico

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
     // Invoco le funzioni per cambiare slide
-    $('.next').click(nextSlide);        // Al click richiamo la funzione per aggiornare l'immagine visualizzando la successiva
+    $('.next').click(nextSlide);  // Al click richiamo la funzione per aggiornare l'immagine visualizzando la successiva
+    $('.prev').click(prevSlide);  // Al click richiamo la funzione per aggiornare l'immagine visualizzando la successiva
 
-
-    // Definisco le funzioni nextImage e prevImage
+    // Definisco le funzioni nextSlide
     function nextSlide() {
         if ( $('.images img.active').hasClass('last') ) {
 
@@ -31,6 +31,32 @@ $(document).ready(function() {
         }
     }
 
+    // Definisco le funzioni prevSlide
+    function prevSlide() {
+        if ( $('.images img.active').hasClass('first') ) {
+
+            $('.images img.active').removeClass('active');
+            $('.images img.last').addClass('active');
+
+            // Pallini
+            $('.slider-nav i.active').removeClass('active');
+            $('.slider-nav i.last').addClass('active');
+
+        } else {
+            var imgAttiva = $('.images img.active'); // Rimuovo l'active
+            var prossimaImg = $('.images img.active').prev();
+
+            imgAttiva.removeClass('active');
+            prossimaImg.addClass('active');
+
+            // Pallini
+            var pallinoAttivo = $('.slider-nav i.active'); // Rimuovo l'active
+            var prossimaPallino = $('.slider-nav i.active').prev();
+
+            pallinoAttivo.removeClass('active');
+            prossimaPallino.addClass('active');
+        }
+    }
 
 
 
